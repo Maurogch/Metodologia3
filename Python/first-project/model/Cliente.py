85eebae21a9933f6f2de6041fe15040bc04b4654
maurogch <maurogch@gmail.com>
2019-10-16 03:17:12 -0300

    Add toString and fixed email validity check

--- Python/first-project/model/Cliente.py
+++ Python/first-project/model/Cliente.py
@@ -1,14 +1,26 @@
 import re
 
 class Cliente:
-    def __init__(self, nombre, edad, email):
-        self.nombre = None
-        self.edad = None
-        self.email = None
+    def __init__(self, nombre: str, edad: int, email: str):
+        self.nombre = nombre
+        self.edad = edad
+        self.email = email
 
+    """
+    ToString method
+    """
+    def __repr__(self):
+        return f'Cliente(nombre: {self.nombre!r}, edad: {self.edad!r}, email: {self.email!r})'
+
+    """
+    Method will check via regex if mail is valid, if it is not valid match will be none
+    match.group() will return an array of the matches, we don't need that here
+    """
     def check_email(self):
-        try:
-            re.compile('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$')
-            return True
-        except re.error:
+        match = re.search(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', self.email, re.I)
+        if match is None:
             return False
+        else:
+            return True
+
+
