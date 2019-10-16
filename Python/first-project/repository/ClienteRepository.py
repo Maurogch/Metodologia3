f93e5e37c62fe6f25be364c20d73948bcda6bcb8
maurogch <maurogch@gmail.com>
2019-10-16 03:18:18 -0300

    Added typing

--- Python/first-project/repository/ClienteRepository.py
+++ Python/first-project/repository/ClienteRepository.py
@@ -1,17 +1,20 @@
+from model.Cliente import Cliente
+
+
 class ClienteRepository:
     def __init__(self):
         self.list = []
 
-    def add(self, item):
+    def add(self, item: Cliente):
         self.list.append(item)
 
-    def delete(self, item):
+    def delete(self, item: Cliente):
         self.list.remove(item)
 
     def get_all(self):
         return self.list
 
-    def get_by_numero(self, numero):
+    def get_by_numero(self, numero: int):
         for f in self.list:
             if numero == f.numero:
                 return f
