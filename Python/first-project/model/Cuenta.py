b7ecc9fdc4a79553f803a333431759974ec27842
maurogch <maurogch@gmail.com>
2019-10-16 03:17:56 -0300

    add toString, withdraw and deposit methods

--- Python/first-project/model/Cuenta.py
+++ Python/first-project/model/Cuenta.py
@@ -1,12 +1,29 @@
 from datetime import datetime
-from model.Cliente import Cliente
+from .Cliente import Cliente
+import uuid
 
 
 class Cuenta:
-    def __init__(self):
-        self.numero = None
-        self.saldo = None
+    def __init__(self, numero: uuid, saldo: float, cliente: Cliente):
+        self.numero = numero
+        self.saldo = saldo
         self.fecha_hora_alta = datetime.now()
-        self.cliente = Cliente()
+        self.cliente = cliente
 
+    def __repr__(self):
+        return f'Cuenta(numero: {self.numero!r}, saldo: {self.saldo!r}, fecha_hora_alta: ' \
+            f'{str(self.fecha_hora_alta)!r}, cliente: {self.cliente.__repr__()})\n'
 
+    def retiro(self, importe: float):
+        if self.saldo >= importe:
+            self.saldo -= importe
+            return f'${importe} retirado, nuevo saldo: ${self.saldo}'
+        else:
+            return "saldo insuficiente"
+
+    def deposito(self, importe: float):
+        self.saldo += importe
+        return f'${importe} depositado, nuevo saldo: ${self.saldo}'
+
+    def get_saldo(self):
+        return self.saldo
